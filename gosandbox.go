be6e9b8537763b18aee0a541aae057561b04c5ea
Ninjapiraatti <tuoppil@gmail.com>
2020-04-02 10:35:12 +0300

    Added a function to basic outputs

--- gosandbox.go
+++ gosandbox.go
@@ -8,14 +8,23 @@ import (
 )
 
 func basicprints() {
-	gotools.Color(gotools.Green, "Here we go\n")
+	gotools.Color(gotools.Magenta, "Here we go\n")
+
+	separator := "**********"
+	fmt.Printf("\n\n%s gotools.Countletters %s\n\n", separator, separator)
+	fmt.Println(gotools.Countletters("abcdffxxyyz", 'z'))
+
+	fmt.Printf("\n\n%s gotools.Fprime %s\n\n", separator, separator)
+	fmt.Println(gotools.Fprime(7))
+
+	fmt.Printf("\n\n%s gotools.Add %s\n\n", separator, separator)
 	x := 42
 	for i := 0; i < 3; i++ {
 		fmt.Println(gotools.Add(42, x*i))
 	}
 
+	fmt.Printf("\n\n%s Various basic stuff. %s\n\n", separator, separator)
 	animals := [2]string{"Gopher", "Honeybadger"}
-
 	cities := []string{}
 	cities = append(cities, "San Diego", "Mountain View")
 	fmt.Printf("%q", cities)
@@ -26,6 +35,7 @@ func basicprints() {
 	cities2 = append(cities2, otherCities...)
 	fmt.Printf("%q", cities2)
 
+	fmt.Printf("\n\n%s Maps %s\n\n", separator, separator)
 	foods := map[string]int{
 		"Carrot": 50,
 		"Radish": 45,
@@ -35,16 +45,24 @@ func basicprints() {
 		fmt.Printf("\nFood: %s. Value: %d.", key, value)
 	}
 
+	fmt.Printf("\n\n%s gotools.Creature %s\n\n", separator, separator)
 	seepra := gotools.Creature{}
 	seepra.Species = ""
 	seepra.Name = "Sepi Seepra"
 	seepra.Age = 0
 	seepra.IsGoodBoi = true
-
 	fmt.Println(seepra.Name)
 	fmt.Println(animals[1])
 	slicetest()
+
+	fmt.Printf("\n\n%s gotools.Multiply %s\n\n", separator, separator)
 	fmt.Println(gotools.Multiply(2, 6))
+
+	fmt.Printf("\n\n%s gotools.WordsToMap %s\n\n", separator, separator)
+	fmt.Println(gotools.WordsToMap("foo bar buzz foo"))
+
+	fmt.Printf("\n\n%s gotools.IsItFortytwo %s\n\n", separator, separator)
+	fmt.Println(gotools.IsItFortytwo(126))
 
 	//gotools.AddCreatureDB(animal)
 }
